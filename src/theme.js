2248e860c9530fc1dc9c3b2a2992763b07a20537
ncnnghia185 <ncn180501@gmail.com>
2023-12-16 14:19:32 +0700

    App Bar

--- src/theme.js
+++ src/theme.js
@@ -1,24 +1,78 @@
 
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
+import { BorderColor } from '@mui/icons-material'
 
 // Create a theme instance.
 const theme = extendTheme({
-  trello:{
-    appBarHeight:'50px',
+  trello: {
+    appBarHeight: '58px',
     boardBarHeight: '60px'
   },
   colorSchemes: {
     light: {
       palette: {
         primary: teal,
-        secondary : deepOrange
+        secondary: deepOrange
       }
     },
     dark: {
       palette: {
         primary: cyan,
         secondary: orange
+      }
+    }
+  },
+  components: {
+    MuiCssBaseline:{
+      styleOverrides:{
+        body:{
+          '*::-webkit-scrollbar':{
+            width :'8px',
+            height :'8px'
+          },
+          '*::-webkit-scrollbar-thumb':{
+            backgroundColor : '#bdc3c7',
+            borderRadius: '10px'
+          },
+          '*::-webkit-scrollbar-thumb:hover':{
+            backgroundColor : '##00b894'
+          }
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize : '0.875rem'
+        })
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize:'0.875rem',
+          '.MuiOutlinedInput-notchedOutline':{
+            borderColor:theme.palette.primary.light
+          },
+          '&:hover': {
+            '.MuiOutlinedInput-notchedOutline':{
+              borderColor:theme.palette.primary.main
+            }
+          },
+          '& fieldset': {
+            borderWidth:'1px !important'
+          }
+        })
       }
     }
   }
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import svrg from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    svrg()
+  ],
   resolve: {
     alias: [
       { find: '~', replacement: '/src' }
